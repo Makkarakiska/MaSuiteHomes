4245082e649d2f8f387817c157ab266a2afb1f2c
Matias Paavilainen <matias@paavilainen.net>
2020-01-31 00:39:55 +0200

    Cleaned up the code

--- src/main/java/fi/matiaspaavilainen/masuitehomes/bukkit/MaSuiteHomes.java
+++ src/main/java/fi/matiaspaavilainen/masuitehomes/bukkit/MaSuiteHomes.java
@@ -9,8 +9,6 @@ import fi.matiaspaavilainen.masuitehomes.bukkit.commands.HomeCommand;
 import fi.matiaspaavilainen.masuitehomes.bukkit.events.JoinEvent;
 import fi.matiaspaavilainen.masuitehomes.bukkit.events.LeaveEvent;
 import fi.matiaspaavilainen.masuitehomes.core.models.Home;
-import org.bukkit.entity.Player;
-import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -25,14 +23,18 @@ public class MaSuiteHomes extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
         // Create configs
         config.create(this, "homes", "config.yml");
         config.create(this, "homes", "syntax.yml");
         config.create(this, "homes", "gui.yml");
 
-        setupBungee();
-        registerListeners();
+        config.create(this, "homes", "messages.yml");
+        // Register channels
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new HomeMessageListener(this));
+
+        getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
+        getServer().getPluginManager().registerEvents(new LeaveEvent(this), this);
 
         PaperCommandManager manager = new PaperCommandManager(this);
         manager.registerCommand(new HomeCommand(this));
@@ -52,40 +54,5 @@ public class MaSuiteHomes extends JavaPlugin {
         new Updator(getDescription().getVersion(), getDescription().getName(), "60632").checkUpdates();
 
         MaSuiteCore.cooldownService.addCooldownLength("homes", config.load("homes", "config.yml").getInt("cooldown"));
-    }
-
-    public int getMaxHomes(Player p) {
-        int max = 0;
-        for (PermissionAttachmentInfo permInfo : p.getEffectivePermissions()) {
-            String perm = permInfo.getPermission();
-            if (perm.startsWith("masuitehomes.home.limit.")) {
-                String amount = perm.replace("masuitehomes.home.limit.", "");
-                if (amount.equalsIgnoreCase("*")) {
-                    max = -1;
-                    break;
-                }
-                try {
-                    if (Integer.parseInt(amount) > max) {
-                        max = Integer.parseInt(amount);
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("[MaSuite] [Homes] Please check your home limit permissions (Not an integer or *) ");
-                }
-            }
-        }
-        return max;
-    }
-
-    private void setupBungee() {
-        config.create(this, "homes", "messages.yml");
-        // Register channels
-        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
-        getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new HomeMessageListener(this));
-    }
-
-    private void registerListeners() {
-        // Events
-        getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
-        getServer().getPluginManager().registerEvents(new LeaveEvent(this), this);
     }
 }
